fc9f4bd67d5854fdfde767ebc1c380b020602b53
natisaway <168241866+natisaway@users.noreply.github.com>
2025-11-12 11:50:30 -0500

    Delete blokusOnline - RandomAI copy 2 directory

--- blokusOnline - RandomAI copy 2/src/modal.js
+++
@@ -1,5 +0,0 @@
-export function wireModal({ instructionsBtn, modal, closeBtn }){
-instructionsBtn.addEventListener("click", () => modal.style.display = "flex");
-closeBtn.addEventListener("click", () => modal.style.display = "none");
-window.addEventListener("click", (e) => { if (e.target === modal) modal.style.display = "none"; });
-}
